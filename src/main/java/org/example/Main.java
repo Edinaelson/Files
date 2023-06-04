8e2e449b9c7e46332b8ff05d19c626496d15e938
EdinaelsonGaleno <sedinaelson@gmail.com>
2023-06-03 21:29:53 -0300

    add img

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,7 @@
 package org.example;
-import org.example.controller.Files;
+import org.example.controller.Archive;
 public class Main{
     public static void main(String[] args) throws InterruptedException {
-        Files.run();
+        Archive.run();
     }
 }
--- src/main/java/org/example/color/Colors.java
+++ src/main/java/org/example/color/Colors.java
@@ -1,4 +1,4 @@
-package org.example;
+package org.example.color;
 
 public class Colors {
 
